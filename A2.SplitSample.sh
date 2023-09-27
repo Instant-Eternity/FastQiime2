95476c89ce7535da4abf0cf38f14896a237ea76e
Instant-Eternity <80820204+Instant-Eternity@users.noreply.github.com>
2023-09-27 07:15:39 -0500

    Update A2.SplitSample.sh

--- A2.SplitSample.sh
+++ A2.SplitSample.sh
@@ -5,7 +5,6 @@
 # Created Time: Wen  9/27 12:17:42 2023
 #########################################################################
 #!/bin/bash
-
 # Function to display usage information
 usage() {
     echo "Usage: $0 -b <barcode_file> -1 <input_R1_file> -2 <input_R2_file>"
@@ -20,6 +19,47 @@ usage() {
     echo "  -o <output_directory>  Output directory (default: fastq_multx_output/)."
     echo "  -h                  Display this help message."
     exit 1
+}
+
+Version(){ 1.4.3
+
+}
+
+split_marge(barcode.txt, 1.clean.fq.gz, 2.clean.fq.gz) {
+    mkdir fastq_multx_output-1/
+    mkdir fastq_multx_output-2/
+
+    time fastq-multx -B barcode.txt \
+        -m 1 \
+        -b 1.clean.fq.gz 2.clean.fq.gz \
+        -o %.R1.fastq \
+        -o %.R2.fastq
+
+    mv *.fastq fastq_multx_output-1/
+
+    time fastq-multx -B barcode.txt \
+        -m 1 \
+        -b 2.clean.fq.gz 1.clean.fq.gz \
+        -o %.R1.fastq \
+        -o %.R2.fastq
+    mv *.fastq fastq_multx_output-2/
+
+    mkdir fastq_multx_output/
+
+    for i in `ls fastq_multx_output-1/*.R1.fastq`; do a=${i/.R1.fastq/}; a=${a/fastq_multx_output-1\//}; echo "$a"; done > sample.list
+    for i in `cat sample.list`; do echo "cat fastq_multx_output-1/$i.R1.fastq fastq_multx_output-2/$i.R2.fastq > ./fastq_multx_output/$i.R1.fastq"; done > command.combine.R1.list
+    for i in `cat sample.list`; do echo "cat fastq_multx_output-1/$i.R2.fastq fastq_multx_output-2/$i.R1.fastq > ./fastq_multx_output/$i.R2.fastq"; done > command.combine.R2.list
+
+    time sh command.combine.R1.list
+    time sh command.combine.R2.list
+
+    rm -rf ./fastq_multx_output-*
+    for i in `ls fastq_multx_output/*.R2.fastq`; do python cut_reverseBarcode.py ${i} ${i}.fq; done
+    rm ./fastq_multx_output/*.R2.fastq
+    ls ./fastq_multx_output/*.fastq.fq |awk -F ".fq" '{print "mv "$0" "$1$2""}'|bash
+
+    gzip ./fastq_multx_output/*.fastq.fq
+    #sed -i '$d' sample.list
 }
 
 # Set default values
@@ -65,47 +105,3 @@ mkdir -p "$output_directory"
 
 # Main
 split_marge(barcode.txt, 1.clean.fq.gz, 2.clean.fq.gz)
-
-# Example usage:
-# ./your_script.sh -b barcode.txt -1 1.clean.fq.gz -2 2.clean.fq.gz -o fastq_multx_output/
-
-
-
-split_marge(barcode.txt, 1.clean.fq.gz, 2.clean.fq.gz) {
-    mkdir fastq_multx_output-1/
-    mkdir fastq_multx_output-2/
-
-    time fastq-multx -B barcode.txt \
-        -m 1 \
-        -b 1.clean.fq.gz 2.clean.fq.gz \
-        -o %.R1.fastq \
-        -o %.R2.fastq
-
-    mv *.fastq fastq_multx_output-1/
-
-    time fastq-multx -B barcode.txt \
-        -m 1 \
-        -b 2.clean.fq.gz 1.clean.fq.gz \
-        -o %.R1.fastq \
-        -o %.R2.fastq
-    mv *.fastq fastq_multx_output-2/
-
-mkdir fastq_multx_output/
-
-for i in `ls fastq_multx_output-1/*.R1.fastq`; do a=${i/.R1.fastq/}; a=${a/fastq_multx_output-1\//}; echo "$a"; done > sample.list
-
-for i in `cat sample.list`; do echo "cat fastq_multx_output-1/$i.R1.fastq fastq_multx_output-2/$i.R2.fastq > ./fastq_multx_output/$i.R1.fastq"; done > command.combine.R1.list
-
-for i in `cat sample.list`; do echo "cat fastq_multx_output-1/$i.R2.fastq fastq_multx_output-2/$i.R1.fastq > ./fastq_multx_output/$i.R2.fastq"; done > command.combine.R2.list
-
-time sh command.combine.R1.list
-time sh command.combine.R2.list
-
-rm -rf ./fastq_multx_output-*
-for i in `ls fastq_multx_output/*.R2.fastq`; do python cut_reverseBarcode.py ${i} ${i}.fq; done
-rm ./fastq_multx_output/*.R2.fastq
-ls ./fastq_multx_output/*.fastq.fq |awk -F ".fq" '{print "mv "$0" "$1$2""}'|bash
-
-gzip ./fastq_multx_output/*.fastq.fq
-#sed -i '$d' sample.list
-}
