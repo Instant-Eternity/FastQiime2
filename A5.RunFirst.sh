8ff1293076485b1f59e7f3141d9f52bcec66aa08
Instant-Eternity <80820204+Instant-Eternity@users.noreply.github.com>
2023-09-27 22:08:56 -0500

    Update A5.RunFirst.sh

--- A5.RunFirst.sh
+++ A5.RunFirst.sh
@@ -3,10 +3,10 @@ sample=GP
 mkdir result_${sample}
 #-----Import Data-----
 time qiime tools import \
-        --type SampleData[PairedEndSequencesWithQuality] \
-        --input-path $PWD/sample_${sample}.txt \
-        --output-path $PWD/result_${sample}/R1.${sample}.demux.qza\
-        --input-format PairedEndFastqManifestPhred33V2
+	--type SampleData[PairedEndSequencesWithQuality] \
+ 	--input-path $PWD/sample_${sample}.txt \
+  	--output-path $PWD/result_${sample}/R1.${sample}.demux.qza\
+   --input-format PairedEndFastqManifestPhred33V2
 
 #-----Cut primer sequence-----
 time qiime cutadapt trim-paired \
@@ -23,34 +23,34 @@ time qiime demux summarize \
 
 #-----Denoise-----
 time qiime dada2 denoise-paired \
-        --i-demultiplexed-seqs $PWD/result_${sample}/R1.${sample}.paired-end-demux.qza \
-        --p-n-threads 12 \
-        --p-trim-left-f 0 --p-trim-left-r 0 \
-        --p-trunc-len-f 0 --p-trunc-len-r 0 \
-        --o-table $PWD/result_${sample}/R2.${sample}.dada2-table.qza \
-        --o-representative-sequences $PWD/result_${sample}/R2.${sample}.dada2-rep-seqs.qza \
-        --o-denoising-stats $PWD/result_${sample}/R2.${sample}.denoising-stats.qza
+    --i-demultiplexed-seqs $PWD/result_${sample}/R1.${sample}.paired-end-demux.qza \
+    --p-n-threads 12 \
+    --p-trim-left-f 0 --p-trim-left-r 0 \
+    --p-trunc-len-f 0 --p-trunc-len-r 0 \
+	--o-table $PWD/result_${sample}/R2.${sample}.dada2-table.qza \
+ 	--o-representative-sequences $PWD/result_${sample}/R2.${sample}.dada2-rep-seqs.qza \
+  	--o-denoising-stats $PWD/result_${sample}/R2.${sample}.denoising-stats.qza
 
 time qiime metadata tabulate \
 	--m-input-file $PWD/result_${sample}/R2.${sample}.denoising-stats.qza \
 	--o-visualization $PWD/result_${sample}/R2.${sample}.denoising-stats.qzv
 
 time qiime feature-table summarize \
-        --i-table $PWD/result_${sample}/R2.${sample}.dada2-table.qza \
-        --o-visualization $PWD/result_${sample}/R2.${sample}.dada2-table.qzv \
-        --m-sample-metadata-file metadata_${sample}.txt
+	--i-table $PWD/result_${sample}/R2.${sample}.dada2-table.qza \
+ 	--o-visualization $PWD/result_${sample}/R2.${sample}.dada2-table.qzv \
+  	--m-sample-metadata-file metadata_${sample}.txt
 
 time qiime feature-table tabulate-seqs \
-        --i-data $PWD/result_${sample}/R2.${sample}.dada2-rep-seqs.qza \
-        --o-visualization $PWD/result_${sample}/R2.${sample}.dada2-rep-seqs.qzv
+    --i-data $PWD/result_${sample}/R2.${sample}.dada2-rep-seqs.qza \
+    --o-visualization $PWD/result_${sample}/R2.${sample}.dada2-rep-seqs.qzv
 
 #-----Constructing Evolutionary Tree-----
 time qiime phylogeny align-to-tree-mafft-fasttree \
-        --i-sequences $PWD/result_${sample}/R2.${sample}.dada2-rep-seqs.qza \
-        --o-alignment $PWD/result_${sample}/R3.${sample}.aligned-rep-seqs.qza \
-        --o-masked-alignment $PWD/result_${sample}/R3.${sample}.masked-aligned-rep-seqs.qza \
-        --o-tree $PWD/result_${sample}/R3.${sample}.unrooted-tree.qza \
-        --o-rooted-tree $PWD/result_${sample}/R3.${sample}.rooted-tree.qza
+	--i-sequences $PWD/result_${sample}/R2.${sample}.dada2-rep-seqs.qza \
+	--o-alignment $PWD/result_${sample}/R3.${sample}.aligned-rep-seqs.qza \
+	--o-masked-alignment $PWD/result_${sample}/R3.${sample}.masked-aligned-rep-seqs.qza \
+ 	--o-tree $PWD/result_${sample}/R3.${sample}.unrooted-tree.qza \
+  	--o-rooted-tree $PWD/result_${sample}/R3.${sample}.rooted-tree.qza
 
 time qiime tools export \
 	--input-path $PWD/result_${sample}/R2.${sample}.dada2-table.qza \
